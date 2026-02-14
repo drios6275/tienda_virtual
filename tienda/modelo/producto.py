25b86ebe1f8512e895773f43dc7fdf077d7bfe8b
David Rios <yosoydrios@gmail.com>
2026-02-14 15:03:49 -0500

    cree el metodo mostrar_info()

--- tienda/modelo/producto.py
+++ tienda/modelo/producto.py
@@ -3,5 +3,10 @@ class Producto:
         self.nombre = nombre
         self.precio = precio
 
+    def mostrar_info(self) -> str:
+        return f"el producto {self.nombre} tiene precio de {self.precio}"
+
+
+
 
 
